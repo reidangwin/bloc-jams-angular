e7e2631089b7a94a6f9a3a1b09fb90c9ceac8762
Reid Angwin <rangwin@gmail.com>
2017-08-07 10:21:49 -0400

    5 Controllers

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,50 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue'},
+        {title: 'Green', duration: 103.96, audioUrl: 'assets/music/green'},
+        {title: 'Red', duration: 268.45, audioUrl: 'assets/music/red'},
+        {title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
+        {title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
+    ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {title: 'Hello, Operator?', duration: '1:01'},
+        {title: 'Ring, ring, ring', duration: '5:01'},
+        {title: 'Fits in your pocket', duration: '3:21'},
+        {title: 'Can you hear me now?', duration: '3:14'},
+        {title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumMariaDemaio = {
+    title: 'Maria DeMaio',
+    artist: 'Maria DeMaio',
+    label: 'Maria DeMaio',
+    year: '2007',
+    albumArtUrl: 'assets/images/album_covers/maria_demaio_cover.jpg',
+    songs: [
+        {title: 'Her Side', duration: '3:06'},
+        {title: 'Beyond the Pale', duration: '3:38'},
+        {title: 'Champagne & Cigarettes', duration: '4:23'},
+        {title: 'Swell', duration: '3:43'},
+        {title: 'Further Behind', duration: '5:11'},
+        {title: 'In My Head', duration: '3:53'},
+        {title: 'Elanor', duration: '3:57'},
+        {title: 'Fine Line', duration: '3:25'},
+        {title: 'Unglued', duration: '2:48'}
+    ]
+};
